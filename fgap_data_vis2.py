26926275dec2f36b35861b34e843bd1701098481
David Wood <david@Deep-Thought.home>
2020-07-02 17:29:34 +0100

    Charts updated and tidied

--- fgap_data_vis2.py
+++ fgap_data_vis2.py
@@ -37,34 +37,43 @@ data.reset_index(inplace=True)
 # clean money formats
 data['Money in'] = data['Money in'].astype(str)
 data['Money Out'] = data['Money Out'].astype(str)
+data['Balance'] = data['Balance'].astype(str)
 
 
 data['Money in'] = data['Money in'].str.replace('£','')
 data['Money Out'] = data['Money Out'].str.replace('£','')
+data['Balance'] = data['Balance'].str.replace('£','')
 
 data['Money in'] = data['Money in'].str.replace('nan','0')
 data['Money Out'] = data['Money Out'].str.replace('nan','0')
+data['Balance'] = data['Balance'].str.replace('nan','0')
 
 data['Money in'] = data['Money in'].str.replace(',','')
 data['Money Out'] = data['Money Out'].str.replace(',','')
+data['Balance'] = data['Balance'].str.replace(',','')
 
 data['Money in'] = data['Money in'].str.replace('(','-')
 data['Money Out'] = data['Money Out'].str.replace('(','-')
+data['Balance'] = data['Balance'].str.replace('(','-')
 
 data['Money in'] = data['Money in'].str.replace(')','')
 data['Money Out'] = data['Money Out'].str.replace(')','')
+data['Balance'] = data['Balance'].str.replace(')','')
 
 data['Money in'] = data['Money in'].str.strip()
 data['Money Out'] = data['Money Out'].str.strip()
+data['Balance'] = data['Balance'].str.strip()
 
 #  print(data.head())
 
 data['Money in'].fillna(0, inplace=True)
 data['Money Out'].fillna(0, inplace=True)
+data['Balance'].fillna(0, inplace=True)
 
 data['Money in'] = pd.to_numeric(data['Money in'])
 data['Money Out'] = pd.to_numeric(data['Money Out'])
-#  print(data['Money in'].head(20))
+data['Balance'] = pd.to_numeric(data['Balance'])
+#  print(data['Balance'].head(20))
 
 data['Surplus'] = data['Money in'] - data['Money Out']
 
@@ -87,6 +96,8 @@ data_surplus.to_csv(my_path + 'data_surplus.csv')
 
 data_inc = data.groupby('month_year')['Money in'].sum()
 data_exp = data.groupby('month_year')['Outflow'].sum()
+data_balance = data.groupby('month_year')['Balance'].mean()
+
 data.reset_index(inplace=True)
 data.set_index('Year', inplace=True)
 data_5 = data.loc['2015'].groupby('Month')['Money in'].sum()
@@ -103,9 +114,10 @@ data_rentee_v_fitzCAF.reset_index(inplace=True)
 
 data_year_grp = data.groupby(['Month','Year'])['Money in'].sum().unstack()
 
-# data.set_index('Date', inplace=True)
+
 dt = pd.to_datetime('today')
-dt = dt - timedelta(days=365)
+nYears = 3
+dt = dt - timedelta(days=365*nYears)
 dt = dt.replace(day=1)
 data_lastYr = data[data['Date'] > dt]
 data_lastYr.to_csv(my_path+'saved_data.csv')
@@ -125,7 +137,7 @@ data_lastYr_inc_grp = data_lastYr_inc.groupby(['month_year','Category'])['Money 
 data_lastYr_inc_grp.reset_index(inplace=True)
 
 fig1 = plt.figure(figsize = (18,12))
-grid1 = gridspec.GridSpec(nrows=2, ncols=1, figure=fig1)  #(nrows=3, ncols=2, figure=fig1)
+grid1 = gridspec.GridSpec(nrows=3, ncols=1, figure=fig1)  #(nrows=3, ncols=2, figure=fig1)
 
 fig2 = plt.figure(figsize = (18,12))
 grid2 = gridspec.GridSpec(nrows=2, ncols=1, figure=fig2)
@@ -144,9 +156,11 @@ ax5 = fig3.add_subplot(grid3[1, 0])
 ax6 = fig4.add_subplot(grid4[0, 0])
 
 ax11 = fig1.add_subplot(grid1[1, 0])
+ax12 = fig1.add_subplot(grid1[2, 0])
 
 ax1.set_title('Income / Expenditure (Surplus) by year and month')
 ax11.set_title('Income from room rental by year and month')
+ax12.set_title('Cash Balance by year and month')
 
 
 # plot 1
@@ -157,9 +171,12 @@ data_surplus.plot.bar(x='month_year',y='Surplus', ax=ax1, color=color, label='Su
 ax1.legend()
 data_rentee_v_fitzCAF.plot.bar(x='month_year', ax=ax11, stacked=True)
 ax11.set_ylim([0, 10000])
-#  ax11.set_xlim(left=pd.to_datetime('2010-08-01'))
+data_balance.plot.bar(x='month_year', y='Balance', ax=ax12, color='green', alpha=0.75)
+ax12.legend()  #  ncol=3, fancybox=True, shadow=True
+
 
 grid1.tight_layout(fig1)
+fig1.savefig(my_path + 'FitzGAP_Inc_Exp' + '.png')
 
 
 #plot 2
@@ -167,44 +184,43 @@ grid1.tight_layout(fig1)
 data_lastYr_rent.plot.bar(x='month_year', ax=ax2, stacked=True, alpha=0.75)
 ax2.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left',
            ncol=5, mode="expand", borderaxespad=0. )  #  ncol=3, fancybox=True, shadow=True
-ax2.set_xlabel('Rental Income over last year by month', fontsize='large', fontweight='bold')
-
+ax2.set_title("Rental Income over last " + str(nYears) + " years", x=0.5, y=0.9)
 
 # plot 3
 
 data_lastYr_exp.plot.bar(x='month_year', ax=ax3, stacked=True, alpha=0.75)
 ax3.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left',
            ncol=4, mode="expand", borderaxespad=0. )
-ax3.set_xlabel('Expenditure categories over last year', fontsize='large', fontweight='bold')
+ax3.set_title("Expenditure categories over last " + str(nYears) + " years", x=0.5, y=0.9)
 
 grid2.tight_layout(fig2)
-
+fig2.savefig(my_path + 'FitzGAP_Last_Year' + '.png')
 
 # plot 4
 
 data_lastYr_inc_grp.plot.bar(x='month_year', ax=ax4, stacked=True)
 ax4.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left',
            ncol=2, mode="expand", borderaxespad=0. )
-ax4.set_xlabel('Income from Members v all Renters over last year', fontsize='large', fontweight='bold')
-
+ax4.set_title("Income from Members v all Renters over last " + str(nYears) + " years", x=0.5, y=0.9)
 
 # plot 5
 plt.rc('axes', prop_cycle=(cycler('color', ['r', 'g', 'b'])))
 data_lastYr_rentee_v_fitzCAF.plot.bar(x='month_year', ax=ax5, stacked=True)
 ax5.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left',
            ncol=5, mode="expand", borderaxespad=0. )
-ax5.set_xlabel('Income from FitzCAF v Renters over last year', fontsize='large', fontweight='bold')
+ax5.set_title("Income from FitzCAF v Renters over last " + str(nYears) + " years", x=0.5, y=0.9)
 
-grid3.tight_layout(fig2)
+grid3.tight_layout(fig3)
+fig3.savefig(my_path + 'FitzGAP_Renters' + '.png')
 
-#plot 6
+#plot 12
 
-#  data.plot.bar(x='month_year', y='Balance', ax=ax6, alpha=0.75)
-#  ax6.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left',
-#             ncol=5, mode="expand", borderaxespad=0. )  #  ncol=3, fancybox=True, shadow=True
-# ax6.set_xlabel('balance', fontsize='large', fontweight='bold')
+data_balance.plot.bar(x='month_year', y='Balance', ax=ax6, alpha=0.75)
+ax6.legend()  #  ncol=3, fancybox=True, shadow=True
+ax6.set_title("Cash Balance", x=0.5, y=0.9)
 
-#  grid4.tight_layout(fig2)
+grid4.tight_layout(fig4)
+fig4.savefig(my_path + 'FitzGAP_Balance' + '.png')
 
 #  plt.tight_layout()
 plt.show()
